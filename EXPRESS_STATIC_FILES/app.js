7ed99115056f9ecea067c9692a9c17337725a0d2
lagrassanicolas <la.grassa.nicolas@iisgalvanimi.edu.it>
2019-10-15 10:26:00 +0000

    punto3

--- EXPRESS_STATIC_FILES/app.js
+++ EXPRESS_STATIC_FILES/app.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req,res) {
- res.sendFile(__dirname +'index.html');
+
+const path = require ('path');
+
+app.get('/index', function(req,res) {
+ res.sendFile(path.join(__dirname+'/index.html'));
 });
+
+
+app.get('/about', function(req,res) {
+ res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+
+app.get('/sitemap', function(req,res) {
+ res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
